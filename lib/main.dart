b12c58bae6b96c3d3e36d5c2d80c55882daab957
Adhitama Fikri <adhitamafikri.dev@gmail.com>
2022-11-22 00:18:26 +0700

    feat: email verif print

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:bootleg_google_keep_app/pages/register_page.dart';
 import 'package:bootleg_google_keep_app/pages/login_page.dart';
+
+import 'firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +23,37 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
-      home: const LoginPage(),
+      home: const HomePage(),
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+        ),
+        body: FutureBuilder(
+            future: Firebase.initializeApp(
+                options: DefaultFirebaseOptions.currentPlatform),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.done:
+                  final user = FirebaseAuth.instance.currentUser;
+                  final emailVerified = user?.emailVerified ?? false;
+                  if (emailVerified) {
+                    print('Your email is verified');
+                  } else {
+                    print('You need to verify email');
+                  }
+                  return const Text('Done');
+                default:
+                  return const Text('Loading');
+              }
+            }));
   }
 }
