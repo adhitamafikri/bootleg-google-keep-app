7507183d1db3b39717bc1ec3d1ff49a0481aaf64
Adhitama Fikri <adhitamafikri.dev@gmail.com>
2022-11-28 22:06:21 +0700

    feat: create new note page

--- lib/pages/notes/new_note_page.dart
+++ lib/pages/notes/new_note_page.dart
@@ -1,6 +1,6 @@
+import 'package:bootleg_google_keep_app/services/auth/auth_service.dart';
+import 'package:bootleg_google_keep_app/services/notes/notes_service.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class NewNotePage extends StatefulWidget {
   const NewNotePage({Key? key}) : super(key: key);
@@ -10,10 +10,110 @@ class NewNotePage extends StatefulWidget {
 }
 
 class _NewNotePageState extends State<NewNotePage> {
+  NotesDatabase? _note;
+  late final NotesService _notesService;
+
+  late final TextEditingController _titleController;
+  late final TextEditingController _bodyController;
+
+  Future<NotesDatabase> createNewNote() async {
+    final existingNote = _note;
+    if (existingNote != null) {
+      return existingNote;
+    }
+    final currentUser = AuthService.firebase().currentUser!;
+    final email = currentUser.email!;
+    final owner = await _notesService.getUser(email: email);
+    return await _notesService.createNote(
+        owner: owner, title: _titleController.text, body: _bodyController.text);
+  }
+
+  void _deleteNoteIfTextIsEmpty() {
+    final note = _note;
+    if (_titleController.text.isEmpty &&
+        _bodyController.text.isEmpty &&
+        note != null) {
+      _notesService.deleteNote(id: note.id);
+    }
+  }
+
+  void _saveNoteIfTextIsNotEmpty() async {
+    final note = _note;
+    if (_titleController.text.isNotEmpty &&
+        _bodyController.text.isNotEmpty &&
+        note != null) {
+      await _notesService.updateNote(
+          note: note, title: _titleController.text, body: _bodyController.text);
+    }
+  }
+
+  void _textControllerListener() async {
+    final note = _note;
+    if (note == null) {
+      return;
+    }
+
+    final title = _titleController.text;
+    final body = _bodyController.text;
+    await _notesService.updateNote(note: note, title: title, body: body);
+  }
+
+  void setupTextControllerListener() {
+    _titleController.removeListener(_textControllerListener);
+    _bodyController.removeListener(_textControllerListener);
+
+    _titleController.addListener(_textControllerListener);
+    _bodyController.addListener(_textControllerListener);
+  }
+
+  @override
+  void initState() {
+    _notesService = NotesService();
+    _titleController = TextEditingController();
+    _bodyController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _deleteNoteIfTextIsEmpty();
+    _saveNoteIfTextIsNotEmpty();
+    _titleController.dispose();
+    _bodyController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('New Note')),
-        body: const Text('Add note here'));
+      appBar: AppBar(title: const Text('New Note')),
+      body: FutureBuilder(
+          future: createNewNote(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                _note = snapshot.data as NotesDatabase;
+                setupTextControllerListener();
+
+                return Column(
+                  children: [
+                    TextField(
+                        controller: _titleController,
+                        decoration:
+                            const InputDecoration(hintText: 'Your Note Title')),
+                    TextField(
+                      controller: _bodyController,
+                      decoration: const InputDecoration(
+                          hintText: 'Write your note here'),
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                    ),
+                  ],
+                );
+              default:
+                return const CircularProgressIndicator();
+            }
+          }),
+    );
   }
 }
--- lib/pages/notes/notes_page.dart
+++ lib/pages/notes/notes_page.dart
@@ -70,6 +70,7 @@ class _NotesPageState extends State<NotesPage> {
                     builder: (context, snapshot) {
                       switch (snapshot.connectionState) {
                         case ConnectionState.waiting:
+                        case ConnectionState.active:
                           return const Text('Waiting for all notes...');
                         default:
                           return const CircularProgressIndicator();
