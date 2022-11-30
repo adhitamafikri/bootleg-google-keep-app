4eff1799112a317af3f50535689c44a84da41026
Adhitama Fikri <adhitamafikri.dev@gmail.com>
2022-11-30 23:01:28 +0700

    feat: use AuthBloc in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-import 'package:bloc/bloc.dart';
 import 'package:bootleg_google_keep_app/pages/notes/new_note_page.dart';
 import 'package:bootleg_google_keep_app/pages/notes/update_note_page.dart';
-import 'package:bootleg_google_keep_app/services/auth/auth_provider.dart';
 import 'package:bootleg_google_keep_app/services/auth/auth_service.dart';
+import 'package:bootleg_google_keep_app/services/auth/bloc/auth_bloc.dart';
+import 'package:bootleg_google_keep_app/services/auth/bloc/auth_event.dart';
+import 'package:bootleg_google_keep_app/services/auth/bloc/auth_state.dart';
+import 'package:bootleg_google_keep_app/services/auth/firebase_auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:bootleg_google_keep_app/pages/register_page.dart';
 import 'package:bootleg_google_keep_app/pages/login_page.dart';
@@ -29,7 +31,10 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const HomePage(),
+        '/': (context) => BlocProvider<AuthBloc>(
+              create: (context) => AuthBloc(FirebaseAuthProvider()),
+              child: const HomePage(),
+            ),
         loginRoute: (context) => const LoginPage(),
         registerRoute: (context) => const RegisterPage(),
         verifyEmailRoute: (context) => const VerifyEmailPage(),
@@ -41,159 +46,50 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// class HomePage extends StatelessWidget {
-//   const HomePage({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return FutureBuilder(
-//         future: AuthService.firebase().initialize(),
-//         builder: (context, snapshot) {
-//           switch (snapshot.connectionState) {
-//             case ConnectionState.done:
-//               final user = AuthService.firebase().currentUser;
-//               if (user != null) {
-//                 final emailVerified = user.isEmailVerified;
-//                 if (emailVerified) {
-//                   return const NotesPage();
-//                 } else {
-//                   return const VerifyEmailPage();
-//                 }
-//               }
-//               return const LoginPage();
-//             default:
-//               return const CircularProgressIndicator();
-//           }
-//         });
-//   }
-// }
-
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  late final TextEditingController _controller;
-
-  @override
-  void initState() {
-    _controller = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => CounterBloc(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello Bloc'),
-        ),
-        body: BlocConsumer<CounterBloc, CounterState>(
-          listener: (context, state) {
-            _controller.clear();
-          },
-          builder: (context, state) {
-            final invalidValue =
-                (state is CounterStateInvalidNumber) ? state.invalidValue : '';
-            return Column(
-              children: [
-                Text('Current value => ${state.value}'),
-                Visibility(
-                  visible: state is CounterStateInvalidNumber,
-                  child: Text('Invalid input: $invalidValue'),
-                ),
-                TextField(
-                  controller: _controller,
-                  decoration:
-                      const InputDecoration(hintText: 'Enter a number here'),
-                  keyboardType: TextInputType.number,
-                ),
-                Row(
-                  children: [
-                    TextButton(
-                        onPressed: () {
-                          context
-                              .read<CounterBloc>()
-                              .add(DecrementEvent(_controller.text));
-                        },
-                        child: const Text('-')),
-                    TextButton(
-                        onPressed: () {
-                          context
-                              .read<CounterBloc>()
-                              .add(IncrementEvent(_controller.text));
-                        },
-                        child: const Text('+')),
-                  ],
-                )
-              ],
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-@immutable
-abstract class CounterState {
-  final int value;
-  const CounterState(this.value);
-}
-
-class CounterStateValid extends CounterState {
-  const CounterStateValid(int value) : super(value);
-}
-
-class CounterStateInvalidNumber extends CounterState {
-  final String invalidValue;
-  const CounterStateInvalidNumber(
-      {required this.invalidValue, required int previousValue})
-      : super(previousValue);
-}
-
-abstract class CounterEvent {
-  final String value;
-  const CounterEvent(this.value);
-}
-
-class IncrementEvent extends CounterEvent {
-  const IncrementEvent(String value) : super(value);
-}
-
-class DecrementEvent extends CounterEvent {
-  const DecrementEvent(String value) : super(value);
-}
-
-class CounterBloc extends Bloc<CounterEvent, CounterState> {
-  CounterBloc() : super(const CounterStateValid(0)) {
-    on<IncrementEvent>((event, emit) {
-      final integer = int.tryParse(event.value);
-      if (integer == null) {
-        emit(CounterStateInvalidNumber(
-            invalidValue: event.value, previousValue: state.value));
+    context.read<AuthBloc>().add(const AuthEventInitialize());
+    return BlocBuilder<AuthBloc, AuthState>(builder: (context, state) {
+      if (state is AuthStateLoggedIn) {
+        // Navigator.of(context)
+        //     .pushNamedAndRemoveUntil(notesRoute, (route) => false);
+        return const NotesPage();
+      } else if (state is AuthStateNeedsVerification) {
+        // Navigator.of(context)
+        //     .pushNamedAndRemoveUntil(verifyEmailRoute, (route) => false);
+        return const VerifyEmailPage();
+      } else if (state is AuthStateLoggedOut) {
+        // Navigator.of(context)
+        //     .pushNamedAndRemoveUntil(loginRoute, (route) => false);
+        return const LoginPage();
       } else {
-        emit(CounterStateValid(state.value + integer));
+        return const Scaffold(
+          body: CircularProgressIndicator(),
+        );
       }
     });
-    on<DecrementEvent>((event, emit) {
-      final integer = int.tryParse(event.value);
-      if (integer == null) {
-        emit(CounterStateInvalidNumber(
-            invalidValue: event.value, previousValue: state.value));
-      } else {
-        emit(CounterStateValid(state.value - integer));
-      }
-    });
+
+    // return FutureBuilder(
+    //     future: AuthService.firebase().initialize(),
+    //     builder: (context, snapshot) {
+    //       switch (snapshot.connectionState) {
+    //         case ConnectionState.done:
+    //           final user = AuthService.firebase().currentUser;
+    //           if (user != null) {
+    //             final emailVerified = user.isEmailVerified;
+    //             if (emailVerified) {
+    //               return const NotesPage();
+    //             } else {
+    //               return const VerifyEmailPage();
+    //             }
+    //           }
+    //           return const LoginPage();
+    //         default:
+    //           return const CircularProgressIndicator();
+    //       }
+    //     });
   }
 }
